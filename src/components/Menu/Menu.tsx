78715a16caa1bd0cd523c97d00df18472c40ad7a
High-Petrushka <daniilprokopovic12@gmail.com>
2025-07-09 18:17:33 +0300

    Adding main menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,17 @@
+import styles from "./Menu.module.scss";
+
 export default function Menu({ handleStart }) {
   return (
     <>
-      <h1>Menu</h1>
-      <p>press the button to get started!</p>
-      <button>Start</button>
+      <div className={styles.cont}>
+        <div className={styles.cont__text}>
+          <h1 className={styles.title}>Tic Tac Toe<br />Game</h1>
+          <p>press the button to get started!</p>
+        </div>
+        <div className={styles.cont__btn}>
+          <button onClick={() => handleStart()}>Start</button>
+        </div>
+      </div>
     </>
   );
 }
